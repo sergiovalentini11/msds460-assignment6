3819240f012da01970a8cbb69144883b8be949f2
sergiovalentini11 <155390647+sergiovalentini11@users.noreply.github.com>
2024-05-02 22:02:07 -0500

    Create lists for counties and districts

--- integer_programming.py
+++ integer_programming.py
@@ -15,7 +15,7 @@ print(total_pop)
 target_pop = total_pop / 10
 print(target_pop)
 
-# The target pop for each district is 602468. County 53033 (King County) has a pop of 1.8 mil, enough for 3 target populations.
+# The target pop for each district is 602468. County 53033 (King County) has a pop of 1.8 mil, enough for 3 target populations (it contains Seattle).
 # So we should assign 3 districts to this county. 
 
 # 53053 (Pierce County), and 53061 (Snohomish County) also have pops greater than the target pop, so I believe we can auto-assign 
@@ -27,4 +27,10 @@ print(remaining_target_pop)
 
 # The target population for the remaining counties is 572411. We want our 5 remaining districts to have populations that are close to that number,
 # so we will need to include that in our constraints.
+
+# List of the counties
+counties = df['state_county'].tolist()
+
+# List of districts (1st - 5th are accounted for by King, Pierce, and Snohomish counties)
+districts = ['6th', '7th', '8th', '9th', '10th']
 
