bdd02fe17e54e05305ebbd0260eb977da11c52e8
sergiovalentini11 <155390647+sergiovalentini11@users.noreply.github.com>
2024-05-02 21:19:49 -0500

    Correcting comments in integer_programming.py

--- integer_programming.py
+++ integer_programming.py
@@ -15,11 +15,5 @@ print(total_pop)
 target_pop = total_pop / 10
 print(target_pop)
 
-# Target pop = 602469. The largest county is 53033 (aka King County) with 1.8 mil - Seatlle is located here.
-# This is roughly 3 target pops. This means this county alone will take up 3 districts. 
 
-# Additionally, county 53053 (Pierce county) has a pop of 709366, and county 53061 (Snohomish county) has a pop of 639797,
-# which are both enough for a district of their own. 
-
-# This means 5 districts and 3 counties are accounted for in 'fairness'. 
 
